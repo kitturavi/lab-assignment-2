30696067db01664cbad8fdbc985df64beb2b2880
kitturavi <ravi.kit1602@gmail.com>
2021-12-08 03:42:54 +0530

    push lab assignment 2
    program to find the area of circle

---
+++ lab assignment 2/lab assignment 2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_assignment_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the radius of the circle :");
+            double radius = Convert.ToDouble(Console.ReadLine());
+            double area = 3.14 * radius * radius;
+            Console.WriteLine(area);
+            Console.ReadLine();
+        }
+    }
+}
