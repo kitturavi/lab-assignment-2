675f2e751755530c43602188fdb0abb361cb86f1
kitturavi <ravi.kit1602@gmail.com>
2021-12-08 04:17:07 +0530

    push
    program to convert string to integer

---
+++ lab assignment 2/lab assignment 2/StringToInteger.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_assignment_2
+{
+    internal class StringToInteger
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter your age:");
+            int age = int.Parse(Console.ReadLine());
+            Console.WriteLine(age);
+            Console.ReadLine();
+        }
+    }
+}
